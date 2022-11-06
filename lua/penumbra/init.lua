52f80e29636d63f25e53c02088d5776b8228ef7c
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2022-11-06 21:57:39 +0900

    feat: add init, palette

---
+++ lua/penumbra/init.lua
@@ -0,0 +1,127 @@
+local o = vim.o
+local g = vim.g
+local cmd = vim.cmd
+local nvim_set_hl = vim.api.nvim_set_hl
+local tbl_deep_extend = vim.tbl_deep_extend
+
+local palette = require('penumbra.palette')
+
+local DEFAULT_CONFIG = {
+  italic_comment = false,
+  transparent_bg = false,
+  show_end_of_buffer = false,
+  lualine_bg_color = nil,
+  light = false,
+  contrast = nil,
+  colors = palette.contrast,
+  overrides = {},
+}
+
+local TRANSPARENTS = {
+  'Normal',
+  'SignColumn',
+  'NvimTreeNormal',
+  'NvimTreeVertSplit',
+}
+
+local function apply_term_colors(colors)
+  g.terminal_color_0=colors.shade_m
+  g.terminal_color_1=colors.red
+  g.terminal_color_2=colors.green
+  g.terminal_color_3=colors.yellow
+  g.terminal_color_4=colors.purple
+  g.terminal_color_5=colors.magenta
+  g.terminal_color_6=colors.cyan
+  g.terminal_color_7=colors.sun
+  g.terminal_color_8=colors.sun_m
+  g.terminal_color_9=colors.red
+  g.terminal_color_10=colors.green
+  g.terminal_color_11=colors.yellow
+  g.terminal_color_12=colors.blue
+  g.terminal_color_13=colors.magenta
+  g.terminal_color_14=colors.cyan
+  g.terminal_color_15=colors.sun_p
+  g.terminal_color_background=colors.shade
+  g.terminal_color_foreground=colors.sky
+end
+
+--@param configs DefaultConfig
+local function apply(configs)
+  local colors = configs.colors
+  local groups = require('penumbra.groups')
+
+  if configs.contrast == 'plus' then
+   colors = palette.contrast_p
+  elseif configs.contrast == 'plusplus' then
+   colors = palette.contrast_pp
+  end
+
+  if configs.light then
+    local base_palettes = colors
+    colors.sun_p = base_palettes.shade_m
+    colors.sun = base_palettes.shade
+    colors.sun_m = base_palettes.shade_p
+    colors.sky_p = base_palettes.sky_m
+    colors.sky_m = base_palettes.sky_p
+    colors.shade_p = base_palettes.sun_m
+    colors.shade = base_palettes.sun
+    colors.shade_m = base_palettes.sun_p
+  end
+
+  apply_term_colors(colors)
+
+  if configs.transparent_bg then
+    for _, group in ipairs(TRANSPARENTS) do
+      groups[group].bg = nil
+    end
+  end
+
+  for group, setting in pairs(configs.overrides) do
+    groups[group] = setting
+  end
+
+  for group, setting in pairs(groups) do
+    nvim_set_hl(0, group, setting)
+  end
+
+end
+
+local local_configs = DEFAULT_CONFIG
+
+local function setup(configs)
+  if type(configs) == 'table' then
+    local_configs = tbl_deep_extend('force', DEFAULT_CONFIG, configs)
+  end
+end
+
+local function load()
+  if vim.version().minor < 7 then
+    vim.notify_once('[penumbra.nvim]: You must use Neovim v0.7 or higher.')
+    return
+  end
+
+  if g.colors_name then
+    cmd('hi clear')
+  end
+
+  if vim.fn.exists('syntax_on') then
+    cmd('syntax reset')
+  end
+
+  o.background = 'dark'
+  o.termguicolors = true
+  g.colors_name='penumbra'
+
+  apply(local_configs)
+end
+
+return {
+  load = load,
+  setup = setup,
+  configs = function ()
+    return local_configs
+  end,
+  colors = function ()
+    return local_configs.colors
+  end
+}
---
+++ lua/penumbra/palette.lua
@@ -0,0 +1,66 @@
+local M = {}
+
+M.contrast = {
+  sun_p = '#FFFDFB',
+  sun = '#FFF7ED',
+  sun_m = '#F2E6D4',
+  sky_p = '#BEBEBE',
+  sky = '#8F8F8F',
+  sky_m = '#636363',
+  shade_p = '#3E4044',
+  shade = '#303338',
+  shade_m = '#24272B',
+
+  red = '#CA736C',
+  orange = '#BA823A',
+  yellow = '#8D9741',
+  green = '#47A477',
+  cyan = '#00A2AF',
+  blue = '#5794D0',
+  purple = '#9481CC',
+  magenta = '#BC73A4',
+}
+
+M.contrast_p = {
+  sun_p = '#FFFDFB',
+  sun = '#FFF7ED',
+  sun_m = '#F2E6D4',
+  sky_p = '#CECECE',
+  sky = '#9E9E9E',
+  sky_m = '#636363',
+  shade_p = '#3E4044',
+  shade = '#24272B',
+  shade_m = '#181B1F',
+
+  red = '#DF7F68',
+  orange = '#CE9042',
+  yellow = '#9CA748',
+  green = '#50B584',
+  cyan = '#00B3C2',
+  blue = '#61A3E6',
+  purple = '#A48FE1',
+  magenta = '#D080B6',
+}
+
+M.contrast_pp = {
+  sun_p = '#FFFDFB',
+  sun = '#FFF7ED',
+  sun_m = '#F2E6D4',
+  sky_p = '#DEDEDE',
+  sky = '#AEAEAE',
+  sky_m = '#636363',
+  shade_p = '#3E4044',
+  shade = '#181B1F',
+  shade_m = '#0D0F13',
+
+  red = '#F58C81',
+  orange = '#E09F47',
+  yellow = '#A9B852',
+  green = '#54C794',
+  cyan = '#00C4D7',
+  blue = '#6EB2FD',
+  purple = '#B69CF6',
+  magenta = '#E58CC5',
+}
+
+return M
